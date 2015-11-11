782e6d1474b4b813c182c943dca0550700c81976
James Gardner <crungmungus@gmail.com>
2015-11-11 12:22:01 +0000

    Sorted out broken indentation.

--- src/js/app.js
+++ src/js/app.js
@@ -9,12 +9,12 @@ var DashboardPanel = require('./dashboard/components/panel');
 var App = React.createClass({
   render: function() {
     return (
-    	<div>
-	    	<h1>Welcome</h1>
-	    	<Dashboard>
-	     		<DashboardPanel title="Active Agents"><Agents /></DashboardPanel>
-	    	</Dashboard>
-	    </div>
+      <div>
+        <h1>Welcome</h1>
+        <Dashboard>
+          <DashboardPanel title="Active Agents"><Agents /></DashboardPanel>
+        </Dashboard>
+      </div>
     );
   }
 });
