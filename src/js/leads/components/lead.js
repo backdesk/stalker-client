2dbd2afb03d335ff0f57372cf6b6f9d6981a767b
James Gardner <crungmungus@gmail.com>
2015-11-16 15:59:50 +0000

    fixed inverted boolean (left over from testing)

--- src/js/leads/components/lead.js
+++ src/js/leads/components/lead.js
@@ -1,38 +1,37 @@
-var React = require('react'),
-    Reflux = require('reflux');
+    var React = require('react'),
+        Reflux = require('reflux');
 
-var leadStore = require('../stores/lead'),
-    actions = require('../actions');
+    var leadStore = require('../stores/lead'),
+        actions = require('../actions');
 
-var Lead = React.createClass({
-  render : function () {
-    var p = this.props.lead;
+    var Lead = React.createClass({
+      render : function () {
+        var p = this.props.lead;
+        return (
+          <div>
+            <h2>{p.details}</h2>
+            <p>{p.description}</p>
+          </div>
+        );
+      }
+    });
 
-    return (
-      <div>
-        <h2>{p.details}</h2>
-        <p>{p.description}</p>
-      </div>
-    );
-  }
-});
+    module.exports = React.createClass({
+      mixins: [Reflux.connect(leadStore)],
 
-module.exports = React.createClass({
-  mixins: [Reflux.connect(leadStore)],
+      componentDidMount : function () {
+        actions.loadLead(this.props.routeParams.id);
+      },
 
-  componentDidMount : function () {
-    actions.loadLead(this.props.routeParams.id);
-  },
+      render : function () {
+        if (this.state.lead) {
+          return (
+            <Lead lead={this.state.lead} />
+          );
+        }
 
-  render : function () {
-    if (!this.state.lead) {
-      return (
-        <Lead lead={this.state.lead} />
-      );
-    }
-
-    return (
-      <div>Loading</div>
-    );
-  }
-});
+        return (
+          <div>Loading</div>
+        );
+      }
+    });
