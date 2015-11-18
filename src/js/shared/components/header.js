b7658a4e2f685c5406368d00d2b0b0522ee2c4fe
James Gardner <crungmungus@gmail.com>
2015-11-18 16:05:00 +0000

    added some dummy menu options to header

--- src/js/shared/components/header.js
+++ src/js/shared/components/header.js
@@ -5,6 +5,10 @@ module.exports = React.createClass({
     return (
       <header className="nav pure-menu-fixed pure-menu-horizontal">
 		<a className="pure-menu-heading" href="">Meh to deh werld</a>
+		<ul className="nav-options pure-menu-list">
+            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Settings</a></li>
+            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Bai</a></li>
+        </ul>
       </header>
     );
   }
